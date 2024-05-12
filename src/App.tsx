e3cbf3f3d31d3c342f5a2039390350e8a76ec734
Bruno Chaves <chavesbruno699@gmail.com>
2024-05-12 05:59:49 -0300

    feat: project layout 50% ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "./global.css";
+import rocketLogo from "./assets/rocket.svg";
+import ButtonPlusIcon from "./assets/ButtonPlusIcon.svg";
 
-import './global.css';
+import { TodoType } from "./interface/TodoData";
+
+const todos: TodoType[] = [
+  {
+    text: "Atualizar meu portifolio",
+    done: false,
+    id: 1,
+  },
+  {
+    text: "Concluir BootCampCTD",
+    done: false,
+    id: 2,
+  },
+  {
+    text: "Me formar na faculdade",
+    done: false,
+    id: 3,
+  },
+  {
+    text: "Desenvolvedor Web",
+    done: false,
+    id: 4,
+  },
+  {
+    text: "Hello World",
+    done: false,
+    id: 5,
+  },
+];
 
 function App() {
   return (
-    <>
-       
-    </>
-  )
+    <div className="min-h-screen grid grid-rows-app bg-base-gray-600 text-product-blue">
+      <header className="flex justify-center bg-base-gray-700">
+        <div className="flex items-center justify-center gap-3">
+          <figure>
+            <img src={rocketLogo} alt="rocket-logo" />
+          </figure>
+
+          <h1 className="text-3xl font-sans font-extrabold">
+            to<span className="text-product-purple-dark">do</span>
+          </h1>
+        </div>
+      </header>
+      <main className="flex justify-center w-screen">
+        <form className="flex absolute gap-2 w-full max-w-2xl">
+          <input
+            className="w-full p-4 rounded-lg bg-base-gray-500 text-base-gray-100 placeholder:text-base-gray-300 border-none"
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Adicione uma nova tarefa"
+            required
+          />
+          <button
+            type="submit"
+            className="flex justify-center items-center gap-2 bg-product-blue-dark text-base-gray-100 hover:bg-product-blue rounded-lg p-4"
+          >
+            ADD
+            <img src={ButtonPlusIcon} />
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,49 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        app: 'minmax(8rem, 12.5rem) 1fr',
+      },
+
+      maxWidth: {
+        app: '700px'
+      }
+
+    },
+    colors: {
+      product: {
+        'blue': '#4EA8DE',
+        'blue-dark': '#1E6F9F',
+        'purple': '#8284FA',
+        'purple-dark': '#5E60CE'
+      },
+      base: {
+        'gray-700': '#0D0D0D',
+        'gray-600': '#1A1A1A',
+        'gray-500': '#262626',
+        'gray-400': '#333333',
+        'gray-300': '#808080',
+        'gray-200': '#D9D9D9',
+        'gray-100': '#F2F2F2',
+      },
+      feedBack: {
+        'danger': '#E25858'
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    fontSize: {
+      xs:['14px', { lineHeight: '24px', letterSpacing: '-0.03em'}],
+      sm:['16px', { lineHeight: '28px', letterSpacing: '-0.03em'}],
+      lg:['18px', { lineHeight: '28px', letterSpacing: '-0.03em'}],
+      xl:['24px', { lineHeight: '36px', letterSpacing: '-0.03em'}],
+      '2xl':['36px', { lineHeight: '48px', letterSpacing: '-0.032em'}],
+      '3xl':['48px', { lineHeight: '56px', letterSpacing: '-0.032em'}],
+      '4xl':['56px', { lineHeight: '64px', letterSpacing: '-0.032em'}],
+      '5xl':['80px', { lineHeight: '80px', letterSpacing: '-0.032em'}],
+    }
   },
   plugins: [],
 }
