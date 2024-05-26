5aa5f7c329b0c1cce3a48dbbfcc1d3c38b75e588
Bruno Chaves <100075447+bchavs12@users.noreply.github.com>
2024-05-26 10:10:42 -0300

    Update Header.tsx tailwindClass

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.svg"
 
 export const Header = () => {
   return (
-    <header className="flex flex-1 py-20 p-x-[0.625rem] justify-center items-center bg-base-gray-700">
+    <header className="flex flex-1 py-20 px-[0.625rem] justify-center items-center bg-base-gray-700">
       <div className="flex items-center justify-center gap-3">
         <figure>
           <img src={Logo} alt="rocket-logo" />
